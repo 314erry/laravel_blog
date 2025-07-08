cb9b7699969992518b39d6213a22027adc9516f0
314erry <314pierry@gmail.com>
2025-07-08 11:37:04 -0300

    Mostrando data de maneira amigável da database  e trabalhando com linkRoute para as rotas de editar e deletar de um post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,7 +3,31 @@
 @section('title', '| View Post')
 
 @section('content')
-    <h1>{{ $post->title }}</h1>
-
-    <p class="lead">{{ $post->body }}</p>
+    <div class="row">
+        <div class="col-md-8">
+            <h1>{{ $post->title }}</h1>
+            <p class="lead">{{ $post->body }}</p>
+        </div>
+        <div class="col-md-4">
+            <div class="well">
+                <dl class="dl-horizontal">
+                    <dt>Created At:</dt>
+                    <dd>{{ date('F j, Y, g:i a', strtotime($post->created_at)) }}</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                    <dt>Last Updated:</dt>
+                    <dd>{{ date('F j, Y, g:i a', strtotime($post->updated_at)) }}</dd>
+                </dl>
+                <hr>
+                <div class="row">
+                    <div class="col-sm-6">
+                        {!! Html::linkRoute('posts.edit', 'Edit', [$post->id], ['class' => 'btn btn-primary btn-block']) !!}
+                    </div>
+                    <div class="col-sm-6">
+                        {!! Html::linkRoute('posts.destroy', 'Delete', [$post->id], ['class' => 'btn btn-danger btn-block']) !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
