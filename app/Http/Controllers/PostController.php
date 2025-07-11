0efe92fff03a2cdb2ae8aa59d34005f8fbba4aac
314erry <314pierry@gmail.com>
2025-07-11 12:03:10 -0300

    Funcao de deletar posts da database, arrumando o botao de delete para englobar um mini form com um metodo restful DELETE

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -113,6 +113,10 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        $post->delete();
+
+        Session::flash('success', 'This post was successfully deleted');
+        return redirect()->route('posts.index');
     }
 }
